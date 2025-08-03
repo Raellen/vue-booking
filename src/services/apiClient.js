05aab8b88a1fa814b0a4f46bb7983ee00adb1121
Raellen <akagi84345312@gmail.com>
2025-08-03 22:17:16 +0800

    fix: api url

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000/',
-    // baseURL: 'https://my-json-server.typicode.com/raellen/vue-booking', 
+    // baseURL: 'http://localhost:3000/',
+    baseURL: 'https://my-json-server.typicode.com/raellen/vue-booking', 
     headers: { 'Content-Type': 'application/json' }
 });
 
